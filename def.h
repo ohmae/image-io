b9340354b06a7b2bfafcd66e5187d14874bddb04
OHMAE Ryosuke <ryo@mm2d.net>
2018-06-28 19:37:11 +0900

    typo

--- def.h
+++ def.h
@@ -18,7 +18,7 @@
 #define ERR(fmt, ...) fprintf(stderr, "\033[31m[%-15.15s:%4u] " fmt "\033[0m\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #define DBG(fmt, ...) fprintf(stderr, "\033[33m[%-15.15s:%4u] " fmt "\033[0m\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #define LOG(fmt, ...) fprintf(stderr, "[%-15.15s:%4u] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-#define PRT(fmt, ...) fprintf(stderror, fmt, ##__VA_ARGS__)
+#define PRT(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
 #else
 #define ERR(fmt, ...)
 #define DBG(fmt, ...)
